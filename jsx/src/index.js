990a1bb2f084f8ff97316eb10e024871c850d614
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-08-09 17:30:42 -0500

    finished jsx intro

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    return <div>Hi there!</div>
+    const buttonText = { text: 'Click Me!'};
+
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter Name
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    )
 };
 
 
