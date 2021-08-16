abaca0786035624dc4543488e7a9a980b8d566ed
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-08-16 17:08:40 -0500

    almost done with seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { lat: null, errorMessage: '' };
-        
-        
-    }
+    state = { lat: null, errorMessage: '' };
+   
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude })
+            ,
+            err => this.setState({ errorMessage: err.message })
+            
         );
     }
 
@@ -28,10 +23,10 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: { this.state.lat }</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
         
-        return <div>Loading!</div>
+        return <Spinner message="Please accept location request" />;
     }
 }
 
