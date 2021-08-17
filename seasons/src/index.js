0067e70b3a590fc92b6c40e3d772a38d94304a40
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-08-17 14:14:05 -0500

    keeping up to date

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -34,7 +34,7 @@ class App extends React.Component {
 
     render () {
         return (
-        <div className="border red">
+        <div className="border red">()
             {this.renderContent()}
         </div>
         )
