e7242b0e6cca6094947c68a265ebc27239606235
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-08-24 15:46:54 -0500

    updating api fetch request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,19 +3,25 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    state = { images: [] }; 
+
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID ni9Q3sBr72wvfVDJp-eDKW5icC33AwZs3KTJ-DeF-No'
             }
         });
+
+        this.setState({ images: response.data.results });
     }
     
     render () {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
