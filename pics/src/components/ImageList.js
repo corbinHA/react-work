2e359d43bb8af44cacdc5a0766558e8df83d5b60
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-08-26 13:37:45 -0500

    rendered list of images

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const ImageList = (props) => {
+    const images = props.images.map(({ description, id, urls }) => {
+        return <img alt={description} key={id} src={urls.regular} />
+    })
+
+
     return (
         <div>
-            ImageList
+            {images}
         </div>
     )
 }
